180be8c191219434b7c5a91e15a2aaade1a12c22
Sanel Zukan <karijes@users.sourceforge.net>
2012-01-16 13:32:12 +0100

    License and url info

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject hubris "0.1.0"
   :description "Hubris - HBase uber shell"
+  :license "EPL 1.0"
+  :url "http://github.com/sanel/hubris"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.apache.hbase/hbase "0.90.0"]
