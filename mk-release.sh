0f919cd6fc4cf2243e96056ca04b23f10bb56c10
Sanel Zukan <karijes@users.sourceforge.net>
2012-01-17 15:51:00 +0100

    Support for creating big and small releases. Made hubris runner to include exported classes and read $HUBRIS_LIB directory

--- mk-release.sh
+++ mk-release.sh
@@ -2,12 +2,18 @@
 
 version=`head -1 project.clj | awk '{print $3}' | sed 's/"//g'`
 dir="hubris-$version"
+dir_big="$dir-full"
 
 lein uberjar
 
+mkdir -p $dir_big
+cp -R bin commands utils hubris-$version-standalone.jar *.md *.html $dir_big
+tar -czpvf $dir_big.tar.gz $dir_big
+
 mkdir -p $dir
-cp -R bin commands utils hubris-$version-standalone.jar *.md *.html $dir
+cp -R bin commands utils hubris-$version.jar *.md *.html $dir
 tar -czpvf $dir.tar.gz $dir
 
 lein clean
 rm -Rf $dir
+rm -Rf $dir_big
